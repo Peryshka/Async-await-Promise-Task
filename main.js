47efe995738108ab388811f980a88e48a63326d8
Perishte <javascript_9595@bk.ru>
2023-11-15 03:24:16 +0500

    functions with promise and async await

---
+++ main.js
@@ -0,0 +1,67 @@
+//Event
+// document.addEventListener('DOMContentLoaded', showFoxRandomImages());
+//
+// //function for getting data from server and showing it on the web-page with async await and fetch
+// //2) Через специальный синтаксис async/await, тоже с обработчиком ошибок
+// async function showFoxRandomImages() {
+//   try {
+//     let response = await fetch('https://randomfox.ca/floof/');
+//     let data = await response.json();
+//     const link_wrap = document.getElementsByClassName('link-wrap')[0];
+//     const img_wrap = document.querySelector('.img-wrap');
+//     const img = document.createElement('img');
+//     img.classList.add('image');
+//     const link = document.createElement('p');
+//     link.classList.add('link-1');
+//     img.src = data.image;
+//     img.alt = data.link;
+//     img_wrap.prepend(img);
+//     link_wrap.append(img.alt);
+//     return data;
+//   } catch (err) {
+//     err.message = 'Oops! Something went wrong.';
+//     const wrap = document.querySelector('.general-wrap');
+//     if (err) {
+//       wrap.style.display = 'none';
+//       const container1 = document.querySelector('.container');
+//       document.body.style.backgroundColor = 'lightgrey';
+//       container1.prepend(err);
+//     }
+//   }
+// };
+
+
+//1) Традиционным способом, с использованием цепочек промисов, включая обработку ошибок
+//Event
+// document.addEventListener('DOMContentLoaded', showRandomFoxes());
+
+//function to show random images and link of foxes with promise and fetch
+// function showRandomFoxes() {
+//   const fetchPromise = fetch("https://randomfox.ca/floof/"); //
+//   fetchPromise.then(response => {
+//     return response.json();
+//   }).then(data => {
+//     const link_wrap = document.getElementsByClassName('link-wrap')[0];
+//     const img_wrap = document.querySelector('.img-wrap');
+//     const img = document.createElement('img');
+//     img.classList.add('image');
+//     const link = document.createElement('p');
+//     link.classList.add('link-1');
+//     img.src = data.image;
+//     img.alt = data.link;
+//     img_wrap.prepend(img);
+//     link_wrap.append(img.alt);
+//
+//   }).catch((err) => { //Ловим ошибку
+//     err.message = 'Oops! Something went wrong.'; //Выводим текст ошибки
+//     const wrap = document.querySelector('.general-wrap');
+//     if (err) {
+//       wrap.style.display = 'none';
+//       const container1 = document.querySelector('.container');
+//       document.body.style.backgroundColor = 'lightgrey';
+//       container1.prepend(err);
+//     }
+//   })
+// }
+
+
